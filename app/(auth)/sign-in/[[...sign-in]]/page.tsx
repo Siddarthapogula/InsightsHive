ee15e7783b2598496f2a09beb0d46960556b965d
Siddartha pogula <pogulaSiddharth96@gmail.com>
2024-04-26 11:02:36 +0530

    initial

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -5,6 +5,7 @@ export default function Page (){
     return
     <div>
         <h1>hi this is sigin in page. have a look</h1>
+        <h1> above p i have added now, for prac may be.</h1>
         <SignIn/>
     </div>
 }
